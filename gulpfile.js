6dc89b37dd11dad40addb777e054a494b7780e13
Steve Hollinger <staff@alchemygamestudio.com>
2024-05-28 17:30:32 -0500

    Basic gulpfile and jasmine test setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const {task, series} = require('gulp')
+const shell = require('gulp-shell')
 
-exports.default = defaultTask
+task('lint', shell.task(['eslint *.js']))
+task('test', shell.task(['npm test']))
+task('server-start', shell.task(['node index.js']))
+
+exports.default = series('lint', 'test', 'server-start')
